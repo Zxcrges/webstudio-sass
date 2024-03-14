07b66c3cee2852db2b076d3ff9bd29c675c1d7b4
Vera <vira.kyseliova@gmail.com>
2024-03-14 17:24:22 +0100

    14.03.24 hw f2 9 finish

--- js/modal.js
+++ js/modal.js
@@ -5,11 +5,25 @@
     modal: document.querySelector("[data-modal]"),
   };
 
+
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.modal.addEventListener("click", handleBackdrop);
+  window.addEventListener("keydown", handleKeyDown);
+
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
     document.body.classList.toggle("no-scroll");
+  }
+  function handleKeyDown(e) {
+    if (e.code === "Escape") {
+      toggleModal();
+    }
+  }
+  function handleBackdrop(e) {
+    if (e.target === e.currentTarget) {
+      toggleModal();
+    }
   }
 })();
